ad9c06052c5c1df4ba709535d2f08ae11b790cd0
Marlik Almighty <cryptocoin62@gmail.com>
2021-06-22 11:43:48 +0300

    up

--- internal/repository/postgresql/init_database.go
+++ internal/repository/postgresql/init_database.go
@@ -3,9 +3,7 @@ package postgresql
 import (
 	"context"
 	"github.com/MarlikAlmighty/library/internal/config"
-	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/jackc/tern/migrate"
 	"github.com/pkg/errors"
 )
 
@@ -45,33 +43,3 @@ func Init(cnf *config.Config) (*PostGreSQl, error) {
 	return &db, nil
 }
 
-func newMigrate(ctx context.Context, cfg *config.Config) error {
-
-	conn, err := pgx.Connect(ctx, cfg.DB)
-	if err != nil {
-		return errors.Wrap(err, "Unable to connection to database: ")
-	}
-
-	defer conn.Close(ctx)
-
-	migrator, err := migrate.NewMigrator(ctx, conn, "schema_version")
-	if err != nil {
-		return errors.Wrap(err, "Unable to create a migrator: ")
-	}
-
-	err = migrator.LoadMigrations(cfg.PathToMigrate)
-	if err != nil {
-		return errors.Wrap(err, "Unable to load migrations: ")
-	}
-
-	err = migrator.Migrate(ctx)
-	if err != nil {
-		return errors.Wrap(err, "Unable to migrate: ")
-	}
-
-	if _, err := migrator.GetCurrentVersion(ctx); err != nil {
-		return errors.Wrap(err, "Unable to get current schema version: ")
-	}
-
-	return nil
-}
